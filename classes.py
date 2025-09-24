a15b6e700ffd2a7258a7e0041c3cdf25ddff98a5
cauacozz11 <cauacozz@gmail.com>
2025-09-24 19:33:21 -0300

    opa

--- classes.py
+++ classes.py
@@ -1,24 +1,34 @@
 class ItensBiblioteca:
-    def __init__(self, categoria, titulo, autor, editora):
+    def __init__(self, categoria, titulo, editora, preco):
         self.__categoria = categoria
         self.__titulo = titulo
-        self.__autor = autor
         self.__editora = editora
+        self.__preco = preco
         
     def mostrar_item(self):
-        return f"Categoria: {self.__categoria} | Título: {self.__titulo} | Autor: {self.__autor} | Editora: {self.__editora}"    
+        return f"Categoria: {self.__categoria} | Título: {self.__titulo} | Editora: {self.__editora} | Preço: {self.__preco}"    
+    
+
+class Livro(ItensBiblioteca):
+    def __init__(self, categoria, titulo, editora, preco, autor ):
+        super().__init__(categoria, titulo, editora, preco)
+        self.__autor = autor
+        
+    def mostrar_livro(self):
+        return f"{super().mostrar_item()} | Autor: {self.__autor}"
     
 
 
-
-
-class Livros(ItensBiblioteca):
-     def __init__(self, categoria, titulo, autor, editora, preco):
-        super().__init__(categoria, titulo, autor, editora)
-        self.__preco = preco
+class Revista(ItensBiblioteca):
+    def __init__(self, categoria, titulo, editora, preco, edicao):
+        super().__init__(categoria, titulo, editora, preco)
+        self.__edicao = edicao
         
+    def mostrar_revista(self):
+        return f"{super().mostrar_item()} | Edição: {self.__edicao}"        
         
 
+        
 class Pessoa:
     def __init__(self, nome, cpf, telefone):
         self.__nome = nome
@@ -39,20 +49,21 @@ class Bibliotecario(Pessoa):
     def mostrar_informacoes_bibliotecario(self):
         print('INFORMÇÕES DO BIBLIOTECÁRIO')
         print()
-        return super().mostrar_informacoes() + f"| CNPJ:{self.__cnpj}"
+        return f"{super().mostrar_informacoes()} | CNPJ:{self.__cnpj}"
     
 
 class Cliente(Pessoa):
-    def __inti__(self, nome, cpf, telefone):
+    def __init__(self, nome, cpf, telefone, id_cliente):
         super().__init__(nome, cpf, telefone)
+        self.__id_cliente = id_cliente
         
     def mostrar_informacoes_cliente(self):
         print('INFORMAÇÕES DO CLIENTE')
         print()
-        return super().mostrar_informacoes()        
+        return f"{super().mostrar_informacoes()} | Cadastro: {self.__id_cliente}"        
     
     
     
-pessoa1 = Cliente('Cauã', 138468931948, 47999343288)
+pessoa1 = Cliente('Cauã', 138468931948, 47999343288, 675)
 
-print(Cliente.mostrar_informacoes_cliente(pessoa1))    
+print(pessoa1.mostrar_informacoes_cliente())    
