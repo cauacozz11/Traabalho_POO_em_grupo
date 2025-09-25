ec5981145f492bc137e64f3747387d098ae8e475
Eduudc16 <eduardodacosta162003@gmail.com>
2025-09-25 18:27:43 -0300

    setter e property

--- classes.py
+++ classes.py
@@ -88,8 +88,7 @@ class Livro(ItensBiblioteca):
     def mostrar_livro(self):
         return f"{super().mostrar_item()} | Autor: {self.__autor}"
     
-
-
+    
 class Revista(ItensBiblioteca):
     def __init__(self, categoria, titulo, editora, preco, edicao):
         super().__init__(categoria, titulo, editora, preco)
@@ -112,20 +111,21 @@ class Revista(ItensBiblioteca):
         
     def mostrar_revista(self):
         return f"{super().mostrar_item()} | Edição: {self.__edicao}"        
-        
-
-        
+            
+            
+#Classe Pessoa-----------------------------------            
 class Pessoa:
     def __init__(self, nome, cpf, telefone):
         self.__nome = nome
         self.__cpf = cpf
         self.__telefone = telefone
 
-    # --- NOME ---
+    # --- NOME -----------------------------------
     @property
     def nome(self):
         return self.__nome
-
+    
+    #Valida para não passar valores vazio 
     @nome.setter
     def nome(self, valor):
         if isinstance(valor, str) and valor.strip():
@@ -133,10 +133,10 @@ class Pessoa:
         else:
             print("Nome deve ser uma string não vazia")
 
-    # --- CPF ---
+    # --- CPF -----------------------------
     @property
     def cpf(self):
-        return self.__cpf
+        return self.__cpfs
 
     @cpf.setter
     def cpf(self, valor):
